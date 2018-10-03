3886fa1f0e5b77aeb2da7f040d9ab9a4247c356f
raineym5 <raineym5@students.rowan.edu>
2018-10-02 21:25:08 -0400

    updated uart transmit

--- ATmega328p/main.c
+++ ATmega328p/main.c
@@ -5,30 +5,40 @@
 #include <./avr/io.h>
 #include <./util/delay.h>
 
-
 void USART_Init( unsigned int ubrr){
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	}
 void USART_Transmit( uint8_t data ){
-
 	while ( !( UCSR0A & (1<<UDRE0)) );
 	UDR0 = data;
 }
-
 int main(void) {
     uint8_t character;
+    uint8_t receivedchars[50] = 0;
+    uint8_t charcounter=0;
 	DDRB = 0xFF;
 	DDRC = 0xFF;
 	DDRD = 0xFF;
 	USART_Init(MYUBRR);
-
     while (1) {
-        while ( !(UCSR0A & (1<<RXC0)) );
-            character = UDR0;
-        USART_Transmit(character);
-        
+    	charcounter=0;
+    	do{
+		    while ( !(UCSR0A & (1<<RXC0)) );
+		        character = UDR0;
+		    receivedchars[charcounter] = character;
+		    charcounter++;
+        }while(character != 0x0D);
+        //set PWM0 receivedchars[charcounter-2]
+        //set PWM1 receivedchars[charcounter-3]
+        //set PWM2 receivedchars[charcounter-4]
+        charcount-=4;
+        receivedchars[charcounter] = 0x0D;
+        receivedchars[0] = receivedchars[0]-1;
+        for(int i=0;i<charcounter;i++){
+        	USART_Transmit(receivedchars[i]);
+        }
     }
     return 0;
 }
