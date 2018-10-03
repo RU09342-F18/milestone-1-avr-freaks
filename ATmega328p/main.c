99a0adecb61d2bb71d7e050d4520a938bafddbfa
raineym5 <raineym5@students.rowan.edu>
2018-10-03 17:04:24 -0400

    updated pwm code

--- ATmega328p/main.c
+++ ATmega328p/main.c
@@ -14,14 +14,21 @@ void USART_Transmit( uint8_t data ){
 	while ( !( UCSR0A & (1<<UDRE0)) );
 	UDR0 = data;
 }
+void Timer_Init(){
+	TCCR0A = (1<<COM0A1)|(1<<COM0B1)|(1<<WGM01)|(1<<WGM00);//set fast pwm, pwm outputs
+	TCCR0B = (1<<CS01)|(1<<CS00);//enable timer and set clock to IOclk/64
+	TCCR2A = (1<<COM2A1)|(1<<WGM21);//set fast pwm, output pwm on channel a
+	TCCR2B = (1<<CS22);//set clock divider
+}
 int main(void) {
-    uint8_t character;
-    uint8_t receivedchars[50] = 0;
+    volatile uint8_t character;
+    uint8_t receivedchars[50];
     uint8_t charcounter=0;
 	DDRB = 0xFF;
 	DDRC = 0xFF;
 	DDRD = 0xFF;
 	USART_Init(MYUBRR);
+	Timer_Init();
     while (1) {
     	charcounter=0;
     	do{
@@ -30,10 +37,10 @@ int main(void) {
 		    receivedchars[charcounter] = character;
 		    charcounter++;
         }while(character != 0x0D);
-        //set PWM0 receivedchars[charcounter-2]
-        //set PWM1 receivedchars[charcounter-3]
-        //set PWM2 receivedchars[charcounter-4]
-        charcount-=4;
+        OCR0A = receivedchars[charcounter-2];
+        OCR0B = receivedchars[charcounter-3];
+        OCR2A = receivedchars[charcounter-4];
+        charcounter-=4;
         receivedchars[charcounter] = 0x0D;
         receivedchars[0] = receivedchars[0]-1;
         for(int i=0;i<charcounter;i++){
