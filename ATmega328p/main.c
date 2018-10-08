4d4c9f25167c6638f48d2ea7f79b234412459484
raineym5 <raineym5@students.rowan.edu>
2018-10-08 17:13:58 -0400

    fixed bug

--- ATmega328p/main.c
+++ ATmega328p/main.c
@@ -17,12 +17,15 @@ void USART_Transmit( uint8_t data ){
 void Timer_Init(){
 	TCCR0A = (1<<COM0A1)|(1<<COM0B1)|(1<<WGM01)|(1<<WGM00);//set fast pwm, pwm outputs
 	TCCR0B = (1<<CS01)|(1<<CS00);//enable timer and set clock to IOclk/64
-	TCCR2A = (1<<COM2A1)|(1<<WGM21);//set fast pwm, output pwm on channel a
+	TCCR2A = (1<<COM2B1)|(1<<WGM21)|(1<<WGM20);//set fast pwm, output pwm on channel a
 	TCCR2B = (1<<CS22);//set clock divider
+	OCR0A = 255;
+	OCR0B = 255;
+	OCR2B = 255;
 }
 int main(void) {
     volatile uint8_t character;
-    uint8_t receivedchars[50];
+    uint8_t receivedchars[50];//16 nodes
     uint8_t charcounter=0;
 	DDRB = 0xFF;
 	DDRC = 0xFF;
@@ -33,23 +36,30 @@ int main(void) {
     	charcounter=0;
     	do{
 		    while ( !(UCSR0A & (1<<RXC0)) );
-		        character = UDR0;
+		    character = UDR0;
 		    receivedchars[charcounter] = character;
-		    USART_Transmit(receivedchars[charcounter]);
-		    charcounter++;
+		    //USART_Transmit(receivedchars[charcounter]);
+		    if(character == 0x7f){
+        		USART_Transmit(receivedchars[charcounter]);
+		    	charcounter--;//backspace
+		    }else
+		    	charcounter++;
+		    
         }while(character != 0x0D);
-        if(charcounter >= 5){
-        OCR0A = receivedchars[1];
-        OCR0B = receivedchars[2];
-        OCR2A = receivedchars[3];
-        charcounter-=4;
+        if(charcounter >= 0){
+		    OCR0A = 255-receivedchars[1];
+		    OCR0B = 255-receivedchars[2];
+		    OCR2B = 255-receivedchars[3];
+		    
+		    charcounter = 4;	
+        	USART_Transmit(receivedchars[0]-3);
+        	while(receivedchars[charcounter] != 0x0D){
+        		USART_Transmit(receivedchars[charcounter]);
+        		charcounter++;
         }
-        receivedchars[charcounter] = 0x0D;
-        
-        USART_Transmit(receivedchars[0]-3);
-        for(int i=4;i<charcounter;i++){
-        	USART_Transmit(receivedchars[i]);
         }
+
+
     }
     return 0;
 }
